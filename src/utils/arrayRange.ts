85749fb1a267a2d0300f037acdd9efe36f5dd58e
Jason Thompson <jason@jasont.us>
2023-05-27 12:22:04 -0400

    fix TS error

--- src/utils/arrayRange.ts
+++ src/utils/arrayRange.ts
@@ -1,5 +1,5 @@
 export const arrayRange = (start: string, stop: string, step: number) =>
   Array.from(
     { length: (parseInt(stop) - parseInt(start)) / step + 1 },
-    (value, index) => (parseInt(start) + index * step).toString()
+    (_value, index) => (parseInt(start) + index * step).toString()
   )
